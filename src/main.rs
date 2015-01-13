3d9199c435cb571c962925615c43628442d07d5a
Jeffrey Zhang <caretcaret@gmail.com>
2015-01-12 20:27:45 -0500

    Bump to 1.0.0-alpha

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,12 @@
-#![feature(phase)]
-
+extern crate "rustc-serialize" as rustc_serialize;
 extern crate docopt;
-#[phase(plugin)] extern crate docopt_macros;
 extern crate image;
-extern crate serialize;
 
+use docopt::Docopt;
 use std::ascii::AsciiExt;
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 use std::fmt::Show;
+use std::hash::Hash;
 use std::io::{fs, File};
 use std::io::fs::PathExtensions;
 use std::path::Path;
@@ -17,14 +16,19 @@ enum SignatureType {
     Constant,
 }
 
-docopt!(Args deriving Show, "
+static USAGE: &'static str = "
 Usage: dedupe [options] <directory>
 
 Options:
     -v, --verbose      Show filenames and hashes during processing.
     --signature TYPE   Use a particular image signature to detect similarity.
                        Valid values: constant [default: constant]
-", flag_signature: SignatureType)
+";
+
+#[derive(RustcDecodable, Show)]
+struct Args {
+    arg_directory: String,
+}
 
 // defines anything that can act as a signature that we can use to compare images,
 // whether that be a locality-sensitive hash or computer vision features, or some
@@ -46,14 +50,41 @@ impl ImageSignature for () {
     fn similarity(&self, other: &()) -> f64 { 1.0f64 }
 }
 
+//struct HashUnionFind<T> {
+//    map: HashMap<T>
+//}
+//
+//trait UnionFind<T: Hash + Eq> {
+//    fn make_set(&self, elt: &T);
+//    fn union(&self, e1: &T, e2: &T);
+//    fn components_with(&self, pred: FnOnce<(HashSet<T>,), bool>)
+//        -> Iterator<HashSet<T>>;
+//
+//    fn components(&self) -> Iterator<HashSet<T>> {
+//        self.components_with(|_| { true })
+//    }
+//}
+//
+//impl<T> UnionFind for HashUnionFind<T> {
+//    fn make_set(&self, elt: &T) {
+//        unimplemented!();
+//    }
+//
+//    fn union(&self, e1: &T, e2: &T) {
+//        unimplemented!();
+//    }
+//
+//    fn components_with(&self, pred: FnOnce<(HashSet<T>,), bool>)
+//        -> Iterator<HashSet<T>> {
+//        unimplemented!();
+//    }
+//}
 
 // extension-based detection of filetype.
 fn supported_extension(path: &Path) -> Option<image::ImageFormat> {
     path.extension_str().and_then(|ext| {
-        match ext.to_ascii_lower().as_slice() {
-            // LZW decoder is broken for gifs
-            // https://github.com/PistonDevelopers/image/issues/91
-            // "gif" => { Some(image::ImageFormat::GIF) },
+        match ext.to_ascii_lowercase().as_slice() {
+            "gif" => { Some(image::ImageFormat::GIF) },
             "png" => { Some(image::ImageFormat::PNG) },
             "png-large" => { Some(image::ImageFormat::PNG) },
             "jpg" => { Some(image::ImageFormat::JPEG) },
@@ -80,7 +111,9 @@ fn process_image<T: ImageSignature>(
 
 fn main() {
     // parse args
-    let args: Args = Args::docopt().decode().unwrap_or_else(|e| e.exit());
+    let args: Args = Docopt::new(USAGE)
+        .and_then(|d| d.decode())
+        .unwrap_or_else(|e| e.exit());
     let dir = Path::new(&args.arg_directory);
 
     // container for signatures
@@ -98,8 +131,26 @@ fn main() {
             }).map(|sig| {
                 // add it to the hash table
                 signatures.insert(path.clone(), sig);
-                println!("{}: {}", path.display(), sig);
+                println!("{}: {:?}", path.display(), sig);
             });
         }
     }
+
+    //// naive pairwise comparison using union-find
+    //let mut signature_sets = HashUnionFind::new();
+    //for path in signatures.keys() {
+    //    signature_sets.make_set(path);
+    //}
+    //for (path1, hash1) in signatures.iter() {
+    //    for (path2, hash2) in signatures.iter() {
+    //        if path1 == path2 {
+    //            continue
+    //        }
+    //        if hash1.is_similar(&hash2) {
+    //           signatures_sets.union(path1, path2) 
+    //        }
+    //    }
+    //}
+
+    //println!("{}", signature_sets.components_with(|component| component.len() >= 2));
 }
